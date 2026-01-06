f14b1d6f5597fb3bd01c34e6f897687314f05194
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-01-06 10:02:24 -0800

    created home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
 import { useMediaQuery } from "react-responsive";
 
 const App = () => {
 	const isSmallScreen = useMediaQuery({query: '(max-width: 600px)'});
 
 	return (
-		<div className="relative">
+		<div className="relative bg-radial from-darkgray-97 to-darkgray-100">
 			{!isSmallScreen 
 				? <Header/>
 				: <Sidebar/>
 			}
+
+			<Home/>
 		</div>
 	)
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,21 @@
+const Home = () => {
+    return (
+        <div className="h-screen w-full flex justify-center pt-20">
+            <div className="flex items-center gap-10">
+                <div>
+                    headshot
+                </div>
+                <div className="flex flex-col">
+                    <div>
+                        Hanna Nouizi
+                    </div>
+                    <div>
+                        Software Engineer
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
