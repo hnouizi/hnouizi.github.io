e363f0cb1552bc36d5931c2c5571570a1b41ea93
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-01-05 21:16:25 -0800

    implemented expanding & collapsing

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
 
 const Sidebar = () => {
     const tabs = ["Home", "Education", "Experience", "Projects"];
+    const [ isExpanded, setIsExpanded ] = useState<boolean>(false);
 
     return (
         <>
-            <div className="absolute fixed h-screen w-40 flex flex-col gap-5 pl-6 bg-darkgray-100 text-lg font-medium z-1">
+            <div className={`absolute fixed h-screen w-40 flex flex-col gap-5 pl-6 bg-darkgray-100 text-lg font-medium z-1 transition-transform duration-400 ${isExpanded ? "translate-x-0" : "-translate-x-full"}`}>
                 <div className="w-full flex justify-end pt-3 pr-3">
-                    <IoClose className="text-2xl"/>
+                    <button
+                        className="text-2xl hover:text-neutral-300 active:text-neutral-400 transition-colors duration-150"
+                        onClick={() => setIsExpanded(false)}
+                    >
+                        <IoClose/>
+                    </button>
                 </div>
                 {tabs.map((tab) => (
                     <div>
@@ -17,10 +24,13 @@ const Sidebar = () => {
                     </div>
                 ))}
             </div>
-            <div className="absolute fixed w-screen h-screen bg-neutral-100/5"/>
+            <div className={`absolute fixed w-screen h-screen bg-neutral-100/5 transition-opacity duration-400 ${isExpanded ? "opacity-100" : "opacity-0"}`}/>
             <div className="absolute">
-                <button className="ml-4 mt-4">
-                    <IoMenu className="text-4xl"/>
+                <button
+                    className="ml-4 mt-4 text-4xl"
+                    onClick={() => setIsExpanded(true)}
+                >
+                    <IoMenu/>
                 </button>
             </div>
         </>
