e09a13a9a434b21bcdc73ea01d2858cd4b4752b2
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-01-06 10:45:16 -0800

    adjusted headshot width

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ const Home = ({ isSmallScreen }: HomeProps) => {
     return (
         <div className="h-screen w-full flex justify-center items-center">
             <div className={`flex justify-center items-center ${isSmallScreen ? "flex-col gap-10" : "gap-15"}`}>
-                <div className={`${isSmallScreen ? "w-2/3 max-w-130" : "w-80"}`}>
+                <div className={`${isSmallScreen ? "w-2/3 max-w-120" : "w-80"}`}>
                     <img className="rounded-full" src="headshot.png"/>
                 </div>
                 <div className={`flex flex-col ${isSmallScreen && "text-center"}`}>
