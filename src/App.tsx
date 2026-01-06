de3da24af8e5b6d05d409cbbffb9649a5d32acae
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-01-05 18:03:56 -0800

    Create header UI (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div> hello world </div>
+	return (
+		<div className="bg-radial from-neutral-100/3 to-neutral-100/0">
+			<div className="w-screen h-screen pt-5">
+				<div className="w-full flex justify-center">
+					<Header/>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App
+export default App;
