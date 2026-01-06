d109f405c2e2b278a9d09808cef20c36082e2dad
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-01-05 18:38:00 -0800

    replaced header with menu icon for small screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import Header from "./components/Header";
+import { useMediaQuery } from "react-responsive";
+import { IoMenu } from "react-icons/io5";
 
 const App = () => {
+	const isSmallScreen = useMediaQuery({query: '(max-width: 600px)'});
+
 	return (
 		<div className="bg-radial from-neutral-100/3 to-neutral-100/0">
-			<div className="w-screen h-screen pt-5">
-				<div className="w-full flex justify-center">
-					<Header/>
-				</div>
+			{!isSmallScreen 
+				? (
+					<div className="w-full flex justify-center mt-5">
+						<Header/>
+					</div>
+				)
+				: (
+					<div className="ml-4 mt-4">
+						<button>
+							<IoMenu className="text-4xl"/>
+						</button>
+					</div>
+				)
+			}
+
+			{/* Home Page */}
+			<div className="w-screen h-screen">
 			</div>
 		</div>
 	)
