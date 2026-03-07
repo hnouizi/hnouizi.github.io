30ca6b1d78fc7303a302357e581790cf7d408321
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-03-07 12:03:49 -0800

    created projects page

---
+++ src/components/Projects.tsx
@@ -0,0 +1,16 @@
+import Heading from "./Heading";
+
+const Projects = () => {
+    return (
+        <div className="mt-30">
+            <Heading>
+                Projects
+            </Heading>
+            <div>
+                projects
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
