c6cc2a857f8c32199af7c65689010ec788b74d99
hnouizi <146014034+hnouizi@users.noreply.github.com>
2026-09-06 11:07:32 -0700

    Updated experiences & projects (#12)

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -5,7 +5,7 @@ export const experiences = [
         "duration": "Feb 2026 - Present",
         "details": [
             <span>
-                TBD
+                Develop internal <span className="detail-highlight"> full-stack web applications </span> to automate workflows for Ware Malcomb employees
             </span>
         ]
     },
@@ -15,7 +15,7 @@ export const experiences = [
         duration: "June - Nov 2025",
         details: [
             <span>
-                Designed & implemented a <span className="detail-highlight">full-stack web application</span> that details Ware Malcomb's projects and displays their geographical location on a map using <span className="detail-highlight">Mapbox's API</span>
+                Designed & implemented a <span className="detail-highlight">full-stack web application</span> that details Ware Malcomb's projects and displays their geographical location on a map using <span className="detail-highlight">Mapbox</span>
             </span>,
             <span>
                 Completed <span className="detail-highlight">Jira tickets</span> and <span className="detail-highlight">reviewed pull requests</span> for various software engineering projects as needed by the digital transformation team
@@ -34,22 +34,5 @@ export const experiences = [
                 Wrote <span className="detail-highlight">detailed meeting notes</span> during every board and general meeting for future reference
             </span>
         ]
-    },
-    {
-        position: "Secretary",
-        organization: "IVC Circle K International",
-        duration: "Mar 2022 - Mar 2023",
-        details: [
-            <span>
-                Ran <span className="detail-highlight">CKI meetings</span> to  <span className="detail-highlight">promote volunteering events</span> to aid our local community
-            </span>,
-            <span>
-                Wrote <span className="detail-highlight">detailed meeting notes</span> during every board and general meeting for future reference
-            </span>,
-            <span>
-                Completed <span className="detail-highlight">monthly reports</span> using <span className="detail-highlight">Excel</span> tracking club membership, volunteering events, members' service hours, and club finances
-            </span>
-        ]
-    
     }
 ]
